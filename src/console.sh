f207d26535fa9ac280cefebcc928d12d6ea4fc11
EpiostGalax <103282407+EpiostGalax@users.noreply.github.com>
2022-05-04 19:17:36 +0200

    Create console.sh

---
+++ src/console.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if ! [ -r vm-sdk ]; then git submodule update --init --recursive; fi
+
+sdk="${1:-vm-sdk}"
+
+cat <<EOF > $sdk/.zshrc
+# local zshrc for easy start of console
+# usage: ZDOTDIR=/path/to/vm-sdk zsh
+pushd \$ZDOTDIR > /dev/null
+source sdk
+load devuan decode
+popd > /dev/null
+EOF
+
+ZDOTDIR=$sdk zsh
